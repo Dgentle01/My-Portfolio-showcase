778db68c6165c661b1c8d929e2bc0b0c4632c880
Crypto Finance <cryptofinance08@gmail.com>
2025-06-25 16:05:15 +0000

    Add the logo to the website logo also so it shows the logo instead of a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='hsl(0,100%,50%)' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='m12 3-1.9 5.8-5.6 2.2 5.6 2.2L12 19l1.9-5.8 5.6-2.2-5.6-2.2z'/><path d='M5 3v4'/><path d='M19 17v4'/><path d='m5 17 2.8-2.8'/><path d='m19 3-2.8 2.8'/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Oswald:wght@400;500;600;700&display=swap" rel="stylesheet" />
